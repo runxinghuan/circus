3a2b8739e76fbd3712c1d11337149754ee2bdbed
runxinghuan <rxinghuan@gmail.com>
2024-02-15 12:44:36 +0800

    Add all changes

--- src/main/java/circus/Trainer.java
+++ src/main/java/circus/Trainer.java
@@ -1,3 +1,9 @@
+package circus;
+
+import circus.animal.Animal;
+import circus.animal.Bird;
+import circus.animal.Duck;
+
 public class Trainer {
     public static void main(String[] args) {
         Duck d = new Duck();
@@ -15,8 +21,6 @@ public class Trainer {
         train(new Duck());
         // train(new Parrot());
 
-        Animal a2 = new Animal();
-        Bird b2 = new Bird();
 
     }
 
